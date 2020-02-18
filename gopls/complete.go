17e17dfa0f2fbfe2e981f91f362d3dff305d19be
nicolas-martin <nicolas.martin.0986@gmail.com>
2020-02-18 11:11:57 -0500

    separate things by gopls commands

---
+++ gopls/complete.go
@@ -0,0 +1,37 @@
+package gopls
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/nicolas-martin/cube/internal/golang_org_x_tools/lsp/protocol"
+	"github.com/nicolas-martin/cube/internal/types"
+)
+
+// Complete sends a complete request
+func (c *Client) Complete(args ...json.RawMessage) (interface{}, error) {
+	params := &protocol.CompletionParams{
+		TextDocumentPositionParams: protocol.TextDocumentPositionParams{
+			TextDocument: protocol.TextDocumentIdentifier{
+				// NOTE: assume 1 file only
+				URI: c.B.Name,
+			},
+			Position: c.Point.ToPosition(),
+		},
+	}
+	res, err := c.Server.Completion(context.Background(), params)
+	if err != nil {
+		return nil, fmt.Errorf("called to gopls.Completion failed: %v", err)
+	}
+
+	start := c.Point.Col
+	if len(res.Items) > 0 {
+		pos, err := types.PointFromPosition(c.B, res.Items[0].TextEdit.Range.Start)
+		if err != nil {
+			return nil, fmt.Errorf("failed to derive completion start: %v", err)
+		}
+		start = pos.Col - 1 // see help complete-functions
+	}
+	return start, nil
+}
